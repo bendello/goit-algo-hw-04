ef520b670955a9740dcca23324c711befbc71232
bendello <bendello123@gmail.com>
2024-02-06 13:22:28 +0000

    1

--- HW-4-1.py
+++ HW-4-1.py
@@ -1,2 +1,20 @@
-byte_str = 'some text'.encode()
-print(byte_str)
+def total_salary(hw4_1):
+    try:
+        total_sum = 0
+        count = 0
+        with open(hw4_1, 'r', encoding='utf-8') as file:
+            for line in file:
+                name, salary = line.strip().split(',')
+                total_sum += int(salary)
+                count += 1
+        average_salary = total_sum / count if count else 0
+        return total_sum, average_salary
+    except FileNotFoundError:
+        print("Файл не знайдено.")
+        return 0, 0
+    except Exception as e:
+        print(f"Виникла помилка: {e}")
+        return 0, 0
+
+total, average = total_salary(r"C:\Projects\salary_hw4.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
